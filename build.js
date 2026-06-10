9b760afb0261ccb33ffa6cad0f6745e0edea5f28
Milky2018 <842376130@qq.com>
2026-06-10 17:52:05 +0800

    Migrate module config to moon.mod

--- build.js
+++ build.js
@@ -1,10 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 
-const moduleConfigPath = path.join(__dirname, "moon.mod.json");
-const moduleConfig = JSON.parse(fs.readFileSync(moduleConfigPath, "utf8"));
-const macosPackageName = `${moduleConfig.name}/macos`;
-const examplesUtilPackageName = `${moduleConfig.name}/examples/util`;
+function readModuleName() {
+  const moonModPath = path.join(__dirname, "moon.mod");
+  if (fs.existsSync(moonModPath)) {
+    const source = fs.readFileSync(moonModPath, "utf8");
+    const match = source.match(/^name\s*=\s*"([^"]+)"/m);
+    if (match !== null) {
+      return match[1];
+    }
+  }
+
+  const legacyConfigPath = path.join(__dirname, "moon.mod.json");
+  const legacyConfig = JSON.parse(fs.readFileSync(legacyConfigPath, "utf8"));
+  return legacyConfig.name;
+}
+
+const moduleName = readModuleName();
+const macosPackageName = `${moduleName}/macos`;
+const examplesUtilPackageName = `${moduleName}/examples/util`;
 const macosFrameworkFlags =
   "-framework AppKit -framework Foundation -framework CoreGraphics -framework CoreVideo -framework ApplicationServices -lobjc";
 
